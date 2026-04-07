95d6446b331b62940165a4334c48c8d8107495a8
Kasper <kasper.brinkkjaer@gmail.com>
2026-04-07 09:51:33 +0200

    message ændring

--- models/message.js
+++ models/message.js
@@ -1,8 +1,9 @@
 export default class Message {
-    constructor(message, date, owner) {
+    constructor(message, date, owner, chatID) {
         this.message = message;
         this.date = date;
         this.owner = owner;
+        this.chatID = chatID;
         this.id = Message.id++;
     }
     static id = 0;
